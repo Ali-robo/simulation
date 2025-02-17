729a3c712f088992e8572e23623d6964794c2008
Ali-robo <alexander.neubauer2@gmx.de>
2025-02-17 13:37:29 +0100

    neuer Ablauf in main Schleife

--- Zweimassenschwinger3.m
+++ Zweimassenschwinger3.m
@@ -1,8 +1,5 @@
 clear;
 
-%%
-
-%%
 %Konstanten
 c1 = 400; d1 = 0.2; m1 = 6; c2 = 300; d2 = 0.3; m2 = 6; c3 = 80; d3 = 15;
 
@@ -24,16 +21,8 @@ sys2 = zeros(n,2);
 sys1(1,:) = initial_conditions([1,2]);
 sys2(1,:) = initial_conditions([3,4]);
 
-u = zeros(n+1,1);
-
-%% u bestimmen durch numerisches berechen der ersten Schritte
-ode = @(t,x) [x(2); (-c1 * x(1) -d1 * x(2) + c3*(x(3)-x(1)) + d3 * (x(4) - x(2)))/m1;
-              x(4); (-c2 * x(3) -d2 * x(4) - c3*(x(3)-x(1)) - d3 * (x(4) - x(2)))/m2];
-
-[time_sol,sol] = ode45(ode,[0 h], initial_conditions);
-
-%u(1) =  c3 * (sol(1,3)- sol(1,1)) + d3 * (sol(1,2) - sol(1,4));
-%u(2) =  c3 * (sol(end,3)- sol(end,1)) + d3 * (sol(end,2) - sol(end,4));
+%u = zeros(n+1,1);
+u = zeros(2);
 
 u(1) = c3 * (sys2(1,1) - sys1(1,1)) + d3 * (sys2(1,2) - sys1(1,2)); %Anfang fehlerbehaftet.
 u(2) = u(1);
@@ -46,7 +35,57 @@ ode = @(t,x) [x(2); (-c1 * x(1) -d1 * x(2) + c3*(x(3)-x(1)) + d3 * (x(4) - x(2))
 [time_sol,sol] = ode45(ode,time, initial_conditions);
 
 %Speicher der Zeitmessung
-timeToc = zeros(n-1,1);
+%timeToc = zeros(n-1,1);
+
+%% Force - Force
+
+for index = linspace(1,n,n)
+
+    [sys1(index,:), sys2(index,:),u_now] = cosim_F_F(c1,c2,c3,d1,d2,d3,m1,m2,u(1),u(2),t_sol-h,t_sol,h,initial_conditions);
+
+    u = [u(2),u_now];
+
+    [sys1(index,:), sys2(index,:),u_now] = cosim_F_F(c1,c2,c3,d1,d2,d3,m1,m2,u(1),u(2),t_sol,t_sol,h,initial_conditions);
+
+    u(2) = u_now;
+
+    initial_conditions = [sys1(index,:),sys2(index,:)];
+
+    t_sol = t_sol + h;
+
+    if(mod(index,(n/100))==0)
+        fprintf("|");
+    end
+end
+
+
+%% Force - Displacement
+
+%{
+% u in initila_sonditions speichern
+
+initial = [initial_conditions, u(1), u(2), initial_conditions ]
+
+for index = linspace(1,n,n)
+
+    [sys1(index,:), sys2(index,:),u_now] = cosim_F_D(c1,c2,c3,d1,d2,d3,m1,m2,u(1),u(2),sys1(index-2,1),sys1(index-2, 2),sys1(index-1,1),sys1(index-1, 2), t_sol-h,t_sol,h,initial_conditions);
+
+    u = [u(2),u_now];
+
+    [sys1(index,:), sys2(index,:),u_now] =cosim_F_D(c1,c2,c3,d1,d2,d3,m1,m2,u(1),u(2),sys1(index-1,1),sys1(index-1, 2),sys1(index,1),sys1(index, 2), t_sol,t_sol,h,initial_conditions);
+
+    u(2) = u_now;
+
+    initial_conditions = [sys1(index,:),sys2(index,:)];
+
+    t_sol = t_sol + h;
+
+    if(mod(index,(n/100))==0)
+        fprintf("|");
+    end
+end
+
+
 
 for index = linspace(2, 2*n, 2*n-1)
     tic
@@ -56,7 +95,6 @@ for index = linspace(2, 2*n, 2*n-1)
     
 
     [t,sol1] = ode45(@(t,x) dgl_f(t,x(1),x(2),c1,d1,m1, u(index-1), u(index), t_sol-h, h),[t_sol, t_sol+h], inital1);
-
     [t,sol2] = ode45(@(t,x) dgl_f(t,x(1),x(2),c2,d2,m2,-u(index-1),-u(index), t_sol-h, h),[t_sol, t_sol+h], inital2);
 
     u(index+1) = c3 * (sol2(end,1) - sol1(end,1)) + d3 * (sol2(end,2)- sol1(end,2));
@@ -79,6 +117,7 @@ for index = linspace(2, 2*n, 2*n-1)
 end
 
 mean(timeToc)
+%}
 
 %% plotting
 
@@ -113,6 +152,7 @@ xlabel("time");
 ylabel("x2, v2");
 legend("x2", "v2","x1x", "v1n");
 
+%%  Functions and DGL
 
 
 function dxdt = dgl_f(t,x,v,c,d,m, u0, u1, t0,h)
@@ -138,55 +178,50 @@ function dxdt = dgl_d(t,x,v,c,d,m,x0,x1,v0,v1,c3,d3,t0,h)
 end
 
 
-function [sys1,sys2,u] = cosim_F_F(c1,c2,c3,d1,d2,d3,m1,m2,u0,u1,t_sol,h,inital)  %sol = [[x1,v1],[x2,v2],u]
 
-   [t,sol] = ode45(@(t,x) dgl_f(t,x(1),x(2),c1,d1,m1,u0,u1,t_sol,h), [t_sol, t_sol + h], inital([1 2]));
+function [sys1,sys2,u] = cosim_F_F(c1,c2,c3,d1,d2,d3,m1,m2,u0,u1,t0,t_sol,h,inital)  %sol = [[x1,v1],[x2,v2],u]
+
+   [t,sol] = ode45(@(t,x) dgl_f(t,x(1),x(2),c1,d1,m1,u0,u1,t0,h), [t_sol, t_sol + h], inital([1 2]));
    sys1 = sol(end,[1 2]);
 
-   [t,sol] = ode45(@(t,x) dgl_f(t,x(1),x(2),c2,d2,m2,u0,u1,t_sol,h), [t_sol, t_sol + h], inital([3 4]));
+   [t,sol] = ode45(@(t,x) dgl_f(t,x(1),x(2),c2,d2,m2,-u0,-u1,t0,h), [t_sol, t_sol + h], inital([3 4]));
    sys2 = sol(end,[1 2]);
 
-   u = c3 * (sys2(1) - sys1(1)) + d3 * (sys2(2) - sys2(1));
+   u = c3 * (sys2(1) - sys1(1)) + d3 * (sys2(2) - sys1(2));
 
 end
 
-function [sys1,sys2,u] = cosim_F_D(c1,c2,c3,d1,d2,d3,m1,m2,u0,u1,x0,x1,v0,v1,t_sol,h,inital)  %sol = [[x1,v1],[x2,v2],u]
+function [sys1,sys2,u] = cosim_F_D(c1,c2,c3,d1,d2,d3,m1,m2,u0,u1,x0,x1,v0,v1,t0,t_sol,h,inital)  %sol = [[x1,v1],[x2,v2],u]
 
-   [t,sol] = ode45(@(t,x) dgl_f(t,x(1),x(2),c1,d1,m1,u0,u1,t_sol,h), [t_sol, t_sol + h], inital([1 2]));
+   [t,sol] = ode45(@(t,x) dgl_f(t,x(1),x(2),c1,d1,m1,u0,u1,t0,h), [t_sol, t_sol + h], inital([1 2]));
    sys1 = sol(end,[1 2]);
 
-   [t,sol] = ode45(@(t,x) dgl_d(t,x(1),x(2),c2,d2,m2,x0,x1,v0,v1,c3,d3,t_sol,h), [t_sol,t_sol + h], inital([3 4]));
+   [t,sol] = ode45(@(t,x) dgl_d(t,x(1),x(2),c2,d2,m2,x0,x1,v0,v1,c3,d3,t0,h), [t_sol,t_sol + h], inital([3 4]));
    sys2 = sol(end,[1 2]);
 
-   u = c3 * (sys2(1) - sys1(1)) + d3 * (sys2(2) - sys2(1));
+   u = c3 * (sys2(1) - sys1(1)) + d3 * (sys2(2) - sys1(2));
 
 end
 
-function [sys1,sys2,u] = cosim_D_F(c1,c2,c3,d1,d2,d3,m1,m2,u0,u1,x0,x1,v0,v1,t_sol,h,inital)  %sol = [[x1,v1],[x2,v2],u]
+function [sys1,sys2,u] = cosim_D_F(c1,c2,c3,d1,d2,d3,m1,m2,u0,u1,x0,x1,v0,v1,t0,t_sol,h,inital)  %sol = [[x1,v1],[x2,v2],u]
 
-    [t,sol] = ode45(@(t,x) dgl_d(t,x(1),x(2),c1,d1,m1,x0,x1,v0,v1,c3,d3,t_sol,h), [t_sol,t_sol + h], inital([3 4]));
+    [t,sol] = ode45(@(t,x) dgl_d(t,x(1),x(2),c1,d1,m1,x0,x1,v0,v1,c3,d3,t0,h), [t_sol,t_sol + h], inital([3 4]));
     sys1 = sol(end,[1 2]);
 
-    [t,sol] = ode45(@(t,x) dgl_f(t,x(1),x(2),c2,d2,m2,u0,u1,t_sol,h), [t_sol, t_sol + h], inital([3 4]));
+    [t,sol] = ode45(@(t,x) dgl_f(t,x(1),x(2),c2,d2,m2,-u0,-u1,t0,h), [t_sol, t_sol + h], inital([3 4]));
     sys2 = sol(end,[1 2]);
 
-    u = c3 * (sys2(1) - sys1(1)) + d3 * (sys2(2) - sys2(1));
+    u = c3 * (sys2(1) - sys1(1)) + d3 * (sys2(2) - sys1(2));
 
 end
 
 function [sys1,sys2] = cosim_D_D(c1,c2,c3,d1,d2,d3,m1,m2,x1_0,x1_1,v1_0,v1_1,x2_0,x2_1,v2_0,v2_1,t_sol,h,inital)  %sol = [[x1,v1],[x2,v2]]
 
-    [t,sol] = ode45(@(t,x) dgl_d(t,x(1),x(2),c1,d1,m1,x1_0,x1_1,v1_0,v1_1,c3,d3,t_sol,h), [t_sol,t_sol + h], inital([3 4]));
+    [t,sol] = ode45(@(t,x) dgl_d(t,x(1),x(2),c1,d1,m1,x1_0,x1_1,v1_0,v1_1,c3,d3,t0,h), [t_sol,t_sol + h], inital([3 4]));
     sys1 = sol(end,[1 2]);
 
-    [t,sol] = ode45(@(t,x) dgl_d(t,x(1),x(2),c2,d2,m2,x2_0,x2_1,v2_0,v2_1,c3,d3,t_sol,h), [t_sol,t_sol + h], inital([3 4]));
+    [t,sol] = ode45(@(t,x) dgl_d(t,x(1),x(2),c2,d2,m2,x2_0,x2_1,v2_0,v2_1,c3,d3,t0,h), [t_sol,t_sol + h], inital([3 4]));
     sys2 = sol(end,[1 2]);
-
-end
-
-function t1 = test(s1,s2, s3)
-
-    t1 = s1 + s2 * s3;
 
 end
 
